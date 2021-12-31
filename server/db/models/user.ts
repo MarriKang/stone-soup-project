4a569bfe1946d1f6045dea2411586ccea1ec3a1c
Marri Kang <kangmarri@gmail.com>
2021-12-31 01:24:33 -0500

    Add specifications for User model

--- server/db/models/user.ts
+++ server/db/models/user.ts
@@ -1,2 +1,60 @@
-import { Model } from "sequelize";
+'use strict';
+import {
+  Model
+} from 'sequelize';
 
+interface UserAttributes {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    id!: string;
+    firstName!: string;
+    lastName!: string;
+    email!: string;
+    password!: string;
+
+    static associate(models: any) {
+      // define association here
+      User.belongsToMany(models.Project, {through: "user_projects", foreignKey: "user_id"})
+    }
+  };
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
