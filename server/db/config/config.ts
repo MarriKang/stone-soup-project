76fa90bbc4488a0f5eb9749fb95e99c347935671
Marri Kang <kangmarri@gmail.com>
2022-01-05 03:51:00 -0500

    Add ssl option

--- server/db/config/config.ts
+++ server/db/config/config.ts
@@ -19,6 +19,13 @@ module.exports = {
     "database": "stone-soup",
     "host": "127.0.0.1",
     "dialect": "postgres",
+    "logging": false,
+        "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false 
+      }
+    },
     "use_env_variable": "DATABASE_URL"
   }
 }
